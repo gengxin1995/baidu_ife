46b9bbfcfaa3fc19982ccb42b8fc486d0838480d
gengxin1995 <525038358@qq.com>
2018-05-10 08:17:00 +0800

    task43

---
+++ task43/task43.js
@@ -0,0 +1,38 @@
+var allImg = $('img');
+
+allImg.map(function (t) {
+    var randomColor = ('00000' + (Math.random() * 0x1000000 << 0).toString(16)).slice(-6);
+    var randomSize = Math.floor(Math.random() * 15 + 1) * 100 + 'X' + Math.floor(Math.random() * 15 + 1) * 100;
+    $(this).attr('src', 'http://placehold.it/' + randomSize + '/' + randomColor + '/fff');
+})
+
+function setSize() {
+    var imgs3 = $('#imgs3 img');
+    for (var i = 0; i < imgs3.length; i++) {
+        var j = $(imgs3.get(i));
+        var jParent = j.parent();
+        if (i === 0) {
+            j.css('width', jParent.width() - jParent.height() * 0.5 - 1 + 'px');
+        } else {
+            j.css('width', jParent.height() * 0.5 + 'px');
+        }
+    }
+
+    var imgs5 = $('#imgs5 img');
+    for (var i = 0; i < imgs5.length; i++) {
+        var j = $(imgs5.get(i));
+        var jParent = j.parent();
+        if (i === 1) {
+            j.css('height', jParent.width() * 1.0 / 3.0 + 'px');
+        }
+        if (i === 4) {
+            j.css('height', jParent.height() - jParent.width() * 1.0 / 3.0 + 'px');
+        }
+    }
+}
+
+$(window).bind({
+    resize: setSize
+})
+
+setSize();
